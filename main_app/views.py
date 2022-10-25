7a4adfea2e3ccca0ae4ece95ea5ca493415d6ede
jurgenstevens <jurgen.stevens@yahoo.com>
2022-10-24 21:41:33 -0500

    Render songs list in song index file.

--- main_app/views.py
+++ main_app/views.py
@@ -4,18 +4,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 class Song:
-    def __init__(self, original_poster, song_name, artist_name, album_name, song_link, attempted_lyrics):
+    def __init__(self, original_poster, song_name, artist_name, album_name, song_link, attempted_lyrics, upvotes):
         self.original_poster = original_poster
         self.song_name = song_name
         self.artist_name = artist_name
         self.album_name = album_name
         self.song_link = song_link
         self.attempted_lyrics = attempted_lyrics
+        self.upvotes = upvotes
 
 songs = [
-    Song('jurgensteves', 'Dejame En Paz', 'Holy Wave', 'Evil Hits', 'https://www.youtube.com/watch?v=-ZGrgna1s3s&ab_channel=HolyWave-Topic', 'N/A'),
-    Song('jurgensteves', 'White Cat', 'White Fence', 'Cyclops Reap', 'https://www.youtube.com/watch?v=MGCquPuLkXY&ab_channel=dankpatrol', 'N/A'),
-    Song('jurgensteves', 'I Am Not A Game', 'Ty Segall & White Fence', 'Hair', 'https://www.youtube.com/watch?v=4DKjkkuSw7M&ab_channel=TySegall-Topic', 'N/A'),
+    Song('jurgenstevens', 'Dejame En Paz', 'Holy Wave', 'Evil Hits', 'https://www.youtube.com/watch?v=-ZGrgna1s3s&ab_channel=HolyWave-Topic', 'N/A', 0),
+    Song('jurgenstevens', 'White Cat', 'White Fence', 'Cyclops Reap', 'https://www.youtube.com/watch?v=MGCquPuLkXY&ab_channel=dankpatrol', 'N/A', 0),
+    Song('jurgenstevens', 'I Am Not A Game', 'Ty Segall & White Fence', 'Hair', 'https://www.youtube.com/watch?v=4DKjkkuSw7M&ab_channel=TySegall-Topic', 'N/A', 0),
 ]
 
 # Define the home view
@@ -29,5 +30,5 @@ def about(request):
 
 def songs_index(request):
     return render(request, 'songs/index.html', {
-        songs
-    })
+        'songs' : songs
+})
