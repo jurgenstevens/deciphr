7fc202b5df4f7d85c94abc3650f49c34c12d3200
jurgenstevens <jurgen.stevens@yahoo.com>
2022-10-24 23:43:40 -0500

    Create Song model in models file.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    original_poster = models.CharField(max_length=100)
+    song_name = models.CharField(max_length=100)
+    artist_name = models.CharField(max_length=100)
+    album_name = models.CharField(max_length=100)
+    song_link = models.CharField(max_length=100)
+    attempted_lyrics = models.TextField(max_length=250)
+    upvotes = models.IntegerField
