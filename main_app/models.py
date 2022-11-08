72195dc6dec1a9ed444d02250bd12c57f6487a3b
jurgenstevens <jurgen.stevens@yahoo.com>
2022-11-08 17:12:27 -0600

    Connect build in User model to Song model.

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 GENRES = (
     ('P', 'Pop'),
@@ -23,6 +24,7 @@ class Song(models.Model):
     attempted_lyrics = models.TextField(max_length=255)
     upvotes = models.IntegerField(null=True)
     # add user later
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.song_name
