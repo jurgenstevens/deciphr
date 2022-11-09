444c66da6f371100ecb4e8bb0a5c42e42c11eb6a
jurgenstevens <jurgen.stevens@yahoo.com>
2022-11-09 13:11:16 -0600

    Set up the signup url and view.

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('songs/create/', views.SongCreate.as_view(), name='songs_create'),
     path('songs/<int:pk>/update/', views.SongUpdate.as_view(), name='songs_update'),
     path('songs/<int:pk>/delete/', views.SongDelete.as_view(), name='songs_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
     # new route used to show a form and create,read/view update and delete comments
     path('songs/<int:song_id>/add_comment/', views.add_comment, name='add_comment'),
     # new route used to show a form and create,read/view update and delete genre
