94ef7102712ac09a4bd5d22abf07972202524a86
jurgenstevens <jurgen.stevens@yahoo.com>
2023-01-12 00:09:23 -0600

    Update build file.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ BASE_DIR=$(cd "$(dirname "$0")" && pwd)
 source /path/to/virtualenv/bin/activate
 
 # Your other commands here
+pip3 install -r requirements.txt
 pip3 install gunicorn
 pip3 install django
 pip3 install python-dotenv
@@ -10,7 +11,6 @@ pip3 install psycopg2
 pip3 install psycopg2-binary
 pip3 install dj_database_url
 pip3 install
-python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
 
