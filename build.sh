8a2b912e4299920c5265655f42f35411d195da56
jurgenstevens <jurgen.stevens@yahoo.com>
2023-01-12 00:48:22 -0600

    Update build file.

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ pip3 install psycopg2-binary
 pip3 install dj_database_url
 pip3 install
 python manage.py createsuperuser
-python manage.py makemigrations main_app
+python manage.py makemigrations --empty main_app
 python manage.py migrate main_app
 python manage.py runserver
 
