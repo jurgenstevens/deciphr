a03f79264e7ec2f22bbaf1fdac6b2d37f80f3e32
jurgenstevens <jurgen.stevens@yahoo.com>
2023-01-11 23:24:41 -0600

    Run install command instead of eight others.

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@ BASE_DIR=$(cd "$(dirname "$0")" && pwd)
 source /path/to/virtualenv/bin/activate
 
 # Your other commands here
-pip install gunicorn
-pip install django
-pip install python-dotenv
-pip install psycopg2
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
+# pip3 install gunicorn
+# pip3 install django
+# pip3 install python-dotenv
+# pip3 install psycopg2
+# pip3 install dj_database_url
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py runserver
+pip3 install
 
 export BASE_DIR
