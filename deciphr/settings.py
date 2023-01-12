08fd00034685bb0a6ff62adc697ef26dbb9476b9
jurgenstevens <jurgen.stevens@yahoo.com>
2023-01-12 15:07:47 -0600

    Include static root to connect CSS to deployed site.

--- deciphr/settings.py
+++ deciphr/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 LOGIN_REDIRECT_URL = '/songs/'
 LOGOUT_REDIRECT_URL = '/'
 
