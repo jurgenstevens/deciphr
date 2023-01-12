0321453d27720cb85cae2adc4951c52e5c6e9dbe
jurgenstevens <jurgen.stevens@yahoo.com>
2023-01-11 22:26:11 -0600

    Update build file with source command to activate virtual environment.

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-pip install -r requirements.txt
+#/bin/bash
+source /path/to/virtualenv/bin/activate
+
+# Your other commands here
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
