83ed643839941374fc1608af9913210c1c6da84f
jurgenstevens <jurgen.stevens@yahoo.com>
2023-01-12 10:47:16 -0600

    Update build file.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 BASE_DIR=$(cd "$(dirname "$0")" && pwd)
 #/bin/bash
-source /path/to/virtualenv/bin/activate
+# source /path/to/virtualenv/bin/activate
 
 # Your other commands here
 python -m pip install --upgrade pip3
